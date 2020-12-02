969c907223fc60af7b9ef5a5175a0af682a5b262
Matt Bernhardt <mjbernha@mit.edu>
2020-12-01 23:26:05 -0500

    Further refactoring of day 1

--- day01/day1refactor.rb
+++ day01/day1refactor.rb
@@ -1,17 +1,20 @@
 # Problem 1 tasked us to look for groups of 2 and 3 numbers.
 # I do not recommend using larger numbers than 3.
-sequence_length = 3
 
 # Map needed because the data is initially strings.
 input = File.read("input1.txt").split.map(&:to_i)
 
 # Combinatorics FTW
-pairs = input.combination(sequence_length).to_a
-
-for i in 0...pairs.size
-	if pairs[i].sum == 2020
-		puts pairs[i].to_s
-		puts pairs[i].inject(:*)
-		# With only one target expected, we could end here?
+# array: a list of numbers, provided by puzzle
+# seq: the length of sequence to test for. 2 = look for pairs, 3 = look for
+#      triples.
+# Note: I'm using combination, not permutation, because the order does not
+# matter in this case.
+def ftw(array, seq=2)
+	array.combination(seq).each do |s|
+		return s.inject(:*) if s.sum == 2020
 	end
 end
+
+puts ftw(input, 2)
+puts ftw(input, 3)
